c8fb9c7d40cc9f39405c17e371e3380955e86f0f
mateuslph <mlptche@gmail.com>
2022-12-15 10:06:45 -0300

    dados da empresa e styles.js progress bar e loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,92 @@
-console.log("hello word!");
+$(document).ready(function () {
+
+    //progress bar
+
+    let containerA = document.getElementById('circleA');
+    let circleA = new ProgressBar.Circle(containerA, {
+        color: '#64DAF9',
+        strokeWidth: 8,
+        duration: 1400,
+        from: { color: '#AAAAAA' },
+        to: { color: '#65DAF9' },
+
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color)
+            let value = Math.round(circle.value() * 60);
+            circle.setText(value);
+        }
+    });
+
+    let containerB = document.getElementById('circleB');
+    let circleB = new ProgressBar.Circle(containerB, {
+        color: '#64DAF9',
+        strokeWidth: 8,
+        duration: 1600,
+        from: { color: '#AAAAAA' },
+        to: { color: '#65DAF9' },
+
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color)
+            let value = Math.round(circle.value() * 240);
+            circle.setText(value);
+        }
+    });
+
+    let containerC = document.getElementById('circleC');
+    let circleC = new ProgressBar.Circle(containerC, {
+        color: '#64DAF9',
+        strokeWidth: 8,
+        duration: 2000,
+        from: { color: '#AAAAAA' },
+        to: { color: '#65DAF9' },
+
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color)
+            let value = Math.round(circle.value() * 32);
+            circle.setText(value);
+        }
+    });
+
+    let containerD = document.getElementById('circleD');
+    let circleD = new ProgressBar.Circle(containerD, {
+        color: '#64DAF9',
+        strokeWidth: 8,
+        duration: 2200,
+        from: { color: '#AAAAAA' },
+        to: { color: '#65DAF9' },
+
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color)
+            let value = Math.round(circle.value() * 5243);
+            circle.setText(value);
+        }
+    });
+
+    // iniciando o loader quando o usuario chega no elemento
+
+    let dataAreaOffset = $('#data-area').offset();
+    let stop = 0;
+
+    $(window).scroll(function (e) {
+
+        let scroll = $(window).scrollTop();
+
+        if (scroll < (dataAreaOffset.top - 200) && stop == 0) {
+            circleA.animate(1.0);
+            circleB.animate(1.0);
+            circleC.animate(1.0);
+            circleD.animate(1.0);
+
+            stop = 1;
+        }
+    });
+
+    // paralax
+
+    setTimeout(function() {
+
+        $('#data-area').parallax( {imageSrc: 'img/cidadeparallax.png'} );
+
+    }, 250);
+
+});
