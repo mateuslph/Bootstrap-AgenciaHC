2e5773d3ac2789e129ce306d35fcd178d5e65d68
mateuslph <mlptche@gmail.com>
2023-06-12 10:20:53 -0300

    scroll funcional

--- js/scripts.js
+++ js/scripts.js
@@ -127,4 +127,38 @@ $(document).ready(function () {
 
     }
 
+    //scloll para secoes
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#sevices-area');
+    let teamSection = $('#team-area');
+    let portifolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+        let btnId = $(this).attr('id');
+        if(btnId == 'about-manu') {
+            scrollTo = aboutSection;
+        } else if (btnId == 'services-menu') {
+            scrollTo = servicesSection;
+        } else if (btnId == 'team-menu') {
+            scrollTo = teamSection;
+        } else if (btnId == 'portifolio-menu') {
+            scrollTo = portifolioSection;
+        } else if (btnId == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top -70
+        }, 1500);
+
+    });    
+
 });
